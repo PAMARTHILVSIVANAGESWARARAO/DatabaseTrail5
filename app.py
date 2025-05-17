ed06aa9e32bff3c7fc7f2eaee3113f26d3468ad3
PAMARTHI L V SIVANAGESWARA RAO <24a85a0506@sves.org.in>
2025-05-17 22:13:52 +0530

    third  commit

--- app.py
+++ app.py
@@ -57,5 +57,5 @@ def logout():
     return redirect(url_for('signin'))
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
